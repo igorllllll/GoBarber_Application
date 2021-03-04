d8dd7b75bd2d83fb4fcfb704c29a5f940c8c5b32
igorllllll <igorcg47@gmail.com>
2021-03-04 20:24:58 -0300

    Atualizando Avatar

--- back-end/src/config/upload.ts
+++ back-end/src/config/upload.ts
@@ -4,9 +4,14 @@ import multer from 'multer';
 import { request } from 'express';
 
 
+const tmpFolder = path.resolve(__dirname,'../', '../', 'tmp');
+
 export default {
+
+    directory: tmpFolder,
+
     storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'tmp'),
+        destination: tmpFolder,
         filename: (request, file, callback) => {
             const fileHash = crypto.randomBytes(10).toString('base64');
             const fileName = `${fileHash}-${file.originalname}`;
